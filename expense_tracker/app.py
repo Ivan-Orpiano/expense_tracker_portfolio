a930514caffda35d896d3a15a838074a6f267c25
Ivan Orpiano <ivanritchelorpiano@gmail.com>
2026-02-12 15:52:22 +0800

    Application initialization

--- expense_tracker/app.py
+++ expense_tracker/app.py
@@ -21,3 +21,24 @@ class Expense(db.Model):
     category = db.Column(db.String(50), nullable = False)
     description = db.Column(db.String(200))
     date = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Expense {self.category}: ${self.amount}>'
+    
+with app.app_context():
+    db.create_all
+    
+@app.route('/')
+def index():
+    # home page to displays all expenses
+    expenses = Expense.query.order_by(Expense.date.desc()).all()
+    
+    total = sum(expense.amount for expense in expenses)
+    
+    return render_template('expense_index.html', expenses=expenses, total=total)
+
+@app.route ('/add', methods = ['GET', 'POST'])
+def add_expense():
+    #for adding new expenses
+    if request.method == 'POST':
+        pass
